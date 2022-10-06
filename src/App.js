26f5f42ef5d1a8c12f27f2208bcae73038574511
Nguyen Quoc Thang <thangnq@YUNERO.LAN>
2022-10-06 16:53:37 +0700

    load model and load texture threejs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Canvas } from "@react-three/fiber";
+import { Environment, OrbitControls } from "@react-three/drei";
+import { Suspense } from "react";
+import Model from "./components/Model";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Canvas>
+        <Suspense fallback={null}>
+          <Model />
+          <OrbitControls autoRotate />
+          <Environment preset="sunset" background />
+        </Suspense>
+      </Canvas>
     </div>
   );
 }
-
-export default App;
